a91667dcc9e53a5439ddfc05a716de8edade6fcc
Alex Jones <jonesax@hush.com>
2015-03-23 14:42:44 +0000

    updates

--- RedWoodTests/CoreTests.cs
+++ RedWoodTests/CoreTests.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.ComponentModel;
-using Autofac;
+﻿using Autofac;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 using RedWood;
 using RedWood.Interface.Debug;
 using IContainer = Autofac.IContainer;
@@ -20,11 +19,29 @@ namespace RedWoodTests
             container.IsRegistered<ILogger>().Should().BeTrue();
         }
 
+        public interface ITestInterface
+        {
+             int GetFoo();
+        };
+        public class TestClass : ITestInterface
+        {
+            public int GetFoo()
+            {
+                return 1;
+            }
+        };
         [TestMethod]
         public void TestContainerRegister()
         {
             IContainer container = new IoC().GetContainer();
-            
+
+            var builder = new ContainerBuilder();         
+            var instance = NSubstitute.Substitute.For<ITestInterface>();
+            instance.GetFoo().Returns(5);
+            builder.RegisterInstance(instance).As<ITestInterface>();
+            builder.Update(container);
+            container.Resolve<ITestInterface>().GetFoo().Should().Be(5);
+
         }
     }
 }
