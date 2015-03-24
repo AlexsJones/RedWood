0d6597990ae265c4337ac9ddc36d8e914f00ddc0
Alex Jones <jonesax@hush.com>
2015-03-24 13:41:18 +0000

    Basic phantomjs test

--- RedWoodIntegrationTests/CoreIntegrationTests.cs
+++ RedWoodIntegrationTests/CoreIntegrationTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing.Printing;
 using Autofac;
+using FluentAssertions;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using RedWood;
@@ -13,13 +14,14 @@ namespace RedWoodIntegrationTests
     [TestFixture]
     public class CoreIntegrationTests
     {
-    
         [Test]
         public void BasicHeadlessDriverUsage()
         {
             var container = new IoC().GetContainer();
             var headless = container.ResolveKeyed<IWebDriver>(BrowserType.PhantomJS);
-            
+            headless.Navigate().GoToUrl("http://www.google.com");
+            Assert.AreEqual(headless.Title,"Google");
+            headless.Quit();
         }
     }
 }
