87f859a88ef9710908830e3dcf4024c4b3cf6f48
Alex Jones <jonesax@hush.com>
2015-03-25 15:38:47 +0000

    Builds nugets

---
+++ BuildAndDeployNuGetPackages.ps1
@@ -0,0 +1,26 @@
+﻿# Define some parameters
+$OutputPath = "BuildNugets"
+$BuildConfiguration = "Debug"
+
+If (Test-Path $OutputPath){
+	Remove-Item $OutputPath -Force -recurse
+}
+New-Item -Path $OutputPath -ItemType directory
+
+# Find all the *.nuspec files and deliberately omit NuGet packages
+$specs = @(Get-ChildItem ".\" -Filter "*.nuspec" -Recurse | `
+  ? { $PSItem.FullName -inotmatch "\\packages\\" } | `
+  % { $PSItem.FullName } `
+)
+ 
+# Find relevant *.csproj files
+foreach ($spec in $specs) {
+  $folder = $(Split-Path $spec -Parent)
+  $prjs = @(Get-ChildItem $folder -Filter "*.csproj" | % { $PSItem.FullName } )
+  if ($prjs.Count -gt 0) {
+    $prj = @($prjs)[0]
+    Start-Process .nuget\NuGet.exe `
+    -ArgumentList @("pack", "-IncludeReferencedProjects", "-o ""$OutputPath""", "-Properties ""Configuration=$BuildConfiguration""", "$prj") `
+    -Wait
+  }
+}
