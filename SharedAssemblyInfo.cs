337898deeca20fdd03de4b92f0aa24d39903e14a
Alex Jones <AlexsJones@users.noreply.github.com>
2016-04-25 12:03:34 +0100

    modified

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,13 +7,13 @@ using log4net.Config;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(true)]
-[assembly: AssemblyVersion("1.0.40")]
+[assembly: AssemblyVersion("1.0.41")]
 // By default, the "Product version" shown in the file properties window is
 // the same as the value specified for AssemblyFileVersionAttribute.
 // Set AssemblyInformationalVersionAttribute to be the same as
 // AssemblyVersionAttribute so that the "Product version" in the file
 // properties window matches the version displayed in the GAC shell extension.
 
-[assembly: AssemblyInformationalVersion("1.0.40")] // a.k.a. "Product version"
+[assembly: AssemblyInformationalVersion("1.0.41")] // a.k.a. "Product version"
 
 [assembly: XmlConfigurator(Watch = true)]
